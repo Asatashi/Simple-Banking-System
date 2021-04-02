a1bd99296f80fd2e375b9cf8f5ac712b33eaac65
Asatashi <76907974+Asatashi@users.noreply.github.com>
2021-04-02 21:32:53 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,33 +5,26 @@ import java.util.Random;
 
 public class Main {
 
-    protected static void createAcc() {
-        Random random = new Random();
-        long max = 9999999999L;
-        long min = 0_000_000_000L;
-        int maxPin = 9999;
-        int minPin = 0_000;
-        long randomAccNum = random.nextInt((int) (max - min)) + min;
-        int Pin = random.nextInt((maxPin - minPin)) + minPin;
-        String wholeAccNum = "400000" + randomAccNum;
+    protected static void createAcc(long wholeAccNum,int Pin) {
         System.out.println("Your card has been created");
         System.out.println("Your card number:");
-        System.out.println("400000" + randomAccNum);
+        System.out.println(wholeAccNum);
         System.out.println("Your card PIN:");
         System.out.println(Pin);
     }
 
-    protected static void logAcc(String wholeAccNum, int Pin) {
+    protected static void logAcc(long wholeAccNum, int Pin) {
         Scanner scanner = new Scanner(System.in);
         boolean isTrue = false;
         while (!isTrue) {
             System.out.println("Enter your card number:");
-            String logAcc = scanner.nextLine();
-            if (wholeAccNum.equals(logAcc)) {
+            long logAcc = scanner.nextLong();
+            if (logAcc == wholeAccNum) {
                 System.out.println("Enter your PIN:");
                 int logPin = scanner.nextInt();
                 if (logPin == Pin) {
                     System.out.println("You have successfully logged in!");
+                    account();
                     isTrue = true;
                 } else {
                     System.out.println("Wrong card number or PIN!");
@@ -44,46 +37,53 @@ public class Main {
 
     protected static void account() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Balance");
-        System.out.println("2. Log out");
-        System.out.println("0. Exit");
-        int menuSwitch = scanner.nextInt();
-        switch (menuSwitch) {
-            case 1:
-                System.out.println("Balance: 0");
-                break;
-            case 2:
-                System.out.println("You have successfully logged out!");
-                break;
-            case 0:
-                System.out.println("Bye!");
-                break;
-            default:
-                break;
+        boolean isTrue = false;
+        while (!isTrue) {
+            System.out.println("1. Balance");
+            System.out.println("2. Log out");
+            System.out.println("0. Exit");
+            int menuSwitch = scanner.nextInt();
+            switch (menuSwitch) {
+                case 1:
+                    System.out.println("Balance: 0");
+                    break;
+                case 2:
+                    System.out.println("You have successfully logged out!");
+                    isTrue = true;
+                    break;
+                case 0:
+                    System.out.println("Bye!");
+                    isTrue = true;
+                    break;
+                default:
+                    break;
+            }
         }
-
     }
 
-    protected static void menu() {
+    protected static void menu(long randomAccNum,int Pin,long wholeAccNum) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Create an account");
-        System.out.println("2. Log into account");
-        System.out.println("0. Exit");
-        int menuSwitch = scanner.nextInt();
-        switch (menuSwitch) {
-            case 1:
-                createAcc();
-                break;
-            case 2:
-                logAcc(wholeAccNum,Pin);
-                break;
-            case 0:
-                System.out.println("Bye!");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Bad input");
-                break;
+        boolean isTrue = false;
+        while (!isTrue) {
+            System.out.println("1. Create an account");
+            System.out.println("2. Log into account");
+            System.out.println("0. Exit");
+            int menuSwitch = scanner.nextInt();
+            switch (menuSwitch) {
+                case 1:
+                    createAcc(wholeAccNum, Pin);
+                    break;
+                case 2:
+                    logAcc(wholeAccNum, Pin);
+                    break;
+                case 0:
+                    System.out.println("Bye!");
+                    isTrue = true;
+                    break;
+                default:
+                    System.out.println("Bad input");
+                    break;
+            }
         }
     }
 
@@ -92,6 +92,14 @@ public class Main {
 
 
     public static void main(String[] args) {
-        menu();
+        Random random = new Random();       //TODO make program output not identical numbers and pin for card
+        long max = 9999999999L;
+        long min = 0_000_000_000L;
+        int maxPin = 9999;
+        int minPin = 0_000;
+        long randomAccNum = random.nextInt((int) (max - min)) + min;
+        int Pin = random.nextInt((maxPin - minPin)) + minPin;
+        long wholeAccNum = 4000000000000000L + randomAccNum;
+        menu(randomAccNum,Pin,wholeAccNum);
     }
 }
